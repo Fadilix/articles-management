edc9da1f632f270c941c10e5600c65f914e38f67
Fadilix <iamfadile2@gmail.com>
2024-01-04 13:50:03 +0000

    refactoring libraries

--- views/PageSucces.java
+++ views/PageSucces.java
@@ -4,7 +4,6 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
